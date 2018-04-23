3999f52c5e53f292063a3bf2f40c21ed3b9cb928
xiyuanpingtadi <xiyuanpingtadi@outlook.com>
2018-04-23 22:30:29 +0800

    支持gif图

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <?php
 include 'phpqrcode/phpqrcode.php';
-    $qrConf = array(
-        'data'  	=> 'http://www.baidu.com',
-        'level'     => 'H',
-        'size'	    => 15,
-        'mode'      => 'background',
-        'other'     => array('filePath' => 'example\example_gif.gif', 
-                             'emoji'    => '😊'
-                        )
-    );
-    if (is_file($qrConf['other']['filePath'])===false) {
-        echo '文件不存在！';
-        exit;
-    }
-    $isPic = exif_imagetype($qrConf['other']['filePath']);
-    if ($isPic === false) {
-        echo '图片格式有误';
-        exit;
-    }
-    $fileInfo = pathinfo($qrConf['other']['filePath']);
-    if ($qrConf['other']['filePath']!='') 
-        $outFile = 'temp'.DIRECTORY_SEPARATOR.md5($fileInfo['filename'].time()).'.'.$fileInfo['extension'];
-    $qrHander = \QRcode::png($qrConf['data'], $outFile, 'H', $qrConf['size'],0,$saveandprint=false,$qrConf['mode'],$qrConf['other']);
+$qrConf = array(
+    'data' => 'http://www.baidu.com',
+    'level' => 'H',
+    'size' => 15,
+    'mode' => 'background',
+    'other' => array('filePath' => 'example\gif.gif',
+        'emoji' => '😊',
+    ),
+);
+if (is_file($qrConf['other']['filePath']) === false) {
+    echo '文件不存在！';
+    exit;
+}
+$isPic = exif_imagetype($qrConf['other']['filePath']);
+if ($isPic === false) {
+    echo '图片格式有误';
+    exit;
+}
+$fileInfo = pathinfo($qrConf['other']['filePath']);
+if ($qrConf['other']['filePath'] != '') {
+    $outFile = dirname(__FILE__) . DIRECTORY_SEPARATOR .'temp' . DIRECTORY_SEPARATOR . md5($fileInfo['filename'] . time()) . '.' . $fileInfo['extension'];
+}
 
-    // echo $qrHander;
-    header("Content-type: image/gif");
-    imagepng($qrHander);
+$qrHander = \QRcode::png($qrConf['data'], $outFile, 'H', $qrConf['size'], 0, $saveandprint = false, $qrConf['mode'], $qrConf['other']);
 
+// echo $qrHander;
+header("Content-type: image/gif");
+imagepng($qrHander);
